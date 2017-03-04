3b938e8763a65162a5994a42d423a958df360037
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-03-04 19:05:02 +0100

    cleanup

--- learning-hazelcast/src/main/kotlin/learning/uniqueid/SecureRandomUniqueIDGenerator.kt
+++ learning-hazelcast/src/main/kotlin/learning/uniqueid/SecureRandomUniqueIDGenerator.kt
@@ -1,12 +1,7 @@
 package learning.uniqueid
 
 import org.apache.commons.codec.digest.DigestUtils
-import java.security.MessageDigest
 import java.security.SecureRandom
-import java.util.*
-import java.util.UUID
-
-
 
 class SecureRandomUniqueIDGenerator : UniqueIDGenerator {
 
@@ -15,24 +10,6 @@ class SecureRandomUniqueIDGenerator : UniqueIDGenerator {
     override fun nextId(): String {
         val randomNum = Integer(SecureRandom.getInstanceStrong().nextInt()).toString()
 
-        val ts = System.currentTimeMillis().toString()
-        val rand = UUID.randomUUID().toString()
         return DigestUtils.sha1Hex(randomNum.toByteArray())
-    }
-
-    private val HEX_CHARS = "0123456789ABCDEF".toCharArray()
-
-    private fun ByteArray.toHex() : String{
-        val result = StringBuffer()
-
-        forEach {
-            val octet = it.toInt()
-            val firstIndex = (octet and 0xF0).ushr(4)
-            val secondIndex = octet and 0x0F
-            result.append(HEX_CHARS[firstIndex])
-            result.append(HEX_CHARS[secondIndex])
-        }
-
-        return result.toString()
     }
 }
